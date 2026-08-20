ace6a35c2e0388938e5435caef94184069201a47
SharathKumarPorubothula <162026810+SharathKumarPorubothula@users.noreply.github.com>
2026-08-20 15:38:26 +0530

    tested bcrypt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import fs from 'fs/promises';
+import bcrypt from 'bcrypt';
 
 dotenv.config();
 
@@ -15,12 +16,15 @@ var middleware=(req,res,next)=>{
 
 app.get('/:age',middleware,async(req,res)=>{
  var name=req.query.name
+ var pass=req.query.password
  var age=req.params.age
  var body=req.body
  var header=req.headers["content-type"]
 
-await fs.writeFile('data.txt',`name:${name},age:${age},body:${JSON.stringify(body)},header:${header}`) 
-var data=await fs.readFile('data.txt','utf-8')
+var hash=await bcrypt.hash(pass,10)
+await fs.writeFile('data.txt',`${hash}`)
+var pass1=await fs.readFile('data.txt','utf-8')
+var data=await bcrypt.compare(pass,pass1)
 
  res.status(200).send(data)
 
